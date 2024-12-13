477c6ba54a15484d960184c4ac2dea6ecb75ac88
naloyd <naloyd@iupui.edu>
2024-12-13 15:23:18 -0500

    crossing fingers

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def send_annotated_image(filename):
     return send_from_directory(ANNOTATED_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
